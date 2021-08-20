4a8881ea9ac36f5ae5a14cb387efe79a9a8579f4
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-20 21:00:34 +0530

    ADDED:Abiity_to_Edit_Conact

--- src/com/Bridgelabz/AddressBook.java
+++ src/com/Bridgelabz/AddressBook.java
@@ -1,12 +1,11 @@
 package com.Bridgelabz;
 
 import java.util.Scanner;
-import java.util.SimpleTimeZone;
 
 public class AddressBook {
+    Scanner sc = new Scanner(System.in);
+    PersonInfo info =new PersonInfo();
     public void getInput() {
-        Scanner sc = new Scanner(System.in);
-        PersonInfo info =new PersonInfo();
         System.out.println("Enter the personal details : ");
         System.out.print("Enter FIRST NAME: ");
         info.setfirstName(sc.next());
@@ -24,12 +23,77 @@ public class AddressBook {
         info.setPhoneNo(sc.next());
         System.out.print("Enter E-MAIL: ");
         info.setEmail(sc.next());
+    }
+    public void display(){
         System.out.println("first Name: " + info.getfirstName()+ "    LastName: "+info.getLastName()+ "\n" + "Address: " +info.getAddress()+"\n"+"City: " +info.getCity() +"\n"
                 + "State: " +info.getState() + "\n" +"Zip: " +info.getZip() + "\n"+ "Phone No: " +info.getPhoneNo() +"\n" +"Email Address: " +info.getEmail());
     }
+    public void editPersonInfo(){
+        System.out.println("What you want to edit");
+        System.out.println("1.Firstname  2.Lastname  3.Address  4.City  5.State  6.ZIP  7.Phone  8.Email");
+        int option = sc.nextInt();
+        switch (option) {
+
+            case 1:
+                System.out.println("Enter New:");
+                info.setfirstName(sc.next());
+                break;
+            case 2:
+                System.out.println("Enter New:");
+                info.setLastName(sc.next());
+                break;
+            case 3:
+                System.out.println("Enter New:");
+                info.setAddress(sc.next());
+                break;
+            case 4:
+                System.out.println("Enter New:");
+                info.setCity(sc.next());
+                break;
+            case 5:
+                System.out.println("Enter New:");
+                info.setState(sc.next());
+                break;
+            case 6:
+                System.out.println("Enter New:");
+                info.setZip(sc.next());
+                break;
+            case 7:
+                System.out.println("Enter New:");
+                info.setPhoneNo(sc.next());
+                break;
+            case 8:
+                System.out.println("Enter New:");
+                info.setEmail(sc.next());
+                break;
+
+        }
+    }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         AddressBook creatContacts = new AddressBook();
-        creatContacts.getInput();
+        System.out.println("What you like to do? \n" +
+                "1. Add contact \n" +
+                "2. Display contact \n" +
+                "3. Edit contact \n" +
+                "0. Exit ");
+        System.out.println("Enter the key -");
+        int option = sc.nextInt();
+        switch (option){
+            case 1:
+                creatContacts.getInput();
+                break;
+            case 2:
+                creatContacts.display();
+                break;
+            case 3:
+                creatContacts.editPersonInfo();
+                break;
+            default:
+                System.exit(0);
+                break;
+        }
+
     }
 }
